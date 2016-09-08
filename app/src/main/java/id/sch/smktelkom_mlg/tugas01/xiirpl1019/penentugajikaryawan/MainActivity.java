be1dd3539601c203cad40c1d7102a273339d17d1
toshiba <gbagoescahyono@gmail.com>
2016-09-08 14:49:42 +0700

    menambahkan Spinner(Tahun Lahir)

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1019/penentugajikaryawan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1019/penentugajikaryawan/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +13,7 @@ public class MainActivity extends AppCompatActivity {
     EditText etNama;
     Button btOK;
     TextView tvHasil;
+    Spinner spTahun;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,17 +22,32 @@ public class MainActivity extends AppCompatActivity {
 
         etNama = (EditText) findViewById(R.id.editTextNama);
         btOK = (Button) findViewById(R.id.buttonOK);
+        spTahun = (Spinner) findViewById(R.id.spinnerTahun);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
-        btOK.setOnClickListener(new View.OnClickListener() {
+        /*btOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                String nama = etNama.getText().toString();
 
-                tvHasil.setText("Nama anda : " + nama);
 
             }
+        });*/
+
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                doClick();
+            }
         });
+    }
+
+    private void doClick() {
+        String nama = etNama.getText().toString();
+        int tahun = Integer.parseInt(spTahun.getSelectedItem().toString());
+        int usia = 2016 - tahun;
+
+        tvHasil.setText("Nama anda : " + nama + "\nAnda lahir tahun : " + spTahun.getSelectedItem().toString() + "\nUmur Anda " + usia);
     }
 }
