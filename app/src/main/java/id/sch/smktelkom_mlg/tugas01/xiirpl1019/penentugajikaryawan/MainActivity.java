9820878bd79b149758d7bb42e9a0d49ac2bb167a
toshiba <gbagoescahyono@gmail.com>
2016-09-09 23:50:39 +0700

    menambahkan checkBox(pendidikan sekolah)#done

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1019/penentugajikaryawan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1019/penentugajikaryawan/MainActivity.java
@@ -6,8 +6,10 @@ import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.Switch;
 import android.widget.TextView;
@@ -23,6 +25,8 @@ public class MainActivity extends AppCompatActivity {
     TextView tvHasil;
     Spinner spTahun, spJabatan;
     RadioButton rbM, rbBM, rbD, rbJ;
+    RadioGroup rgStatus;
+    CheckBox cbSD, cbSMP, cbSMA, cbD, cbS;
 
 
 
@@ -40,6 +44,12 @@ public class MainActivity extends AppCompatActivity {
         rbD = (RadioButton) findViewById(R.id.radioButtonD);
         rbJ = (RadioButton) findViewById(R.id.radioButtonJ);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
+        cbSD = (CheckBox) findViewById(R.id.checkBoxSD);
+        cbSMP = (CheckBox) findViewById(R.id.checkBoxSMP);
+        cbSMA = (CheckBox) findViewById(R.id.checkBoxSMA);
+        cbD = (CheckBox) findViewById(R.id.checkBoxD);
+        cbS = (CheckBox) findViewById(R.id.checkBoxS);
+        rgStatus = (RadioGroup) findViewById(R.id.radioGroup);
 
         /*btOK.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,6 +74,9 @@ public class MainActivity extends AppCompatActivity {
 
     private void doClick() {
         String nama = etNama.getText().toString();
+
+        
+        
         int tahun = Integer.parseInt(spTahun.getSelectedItem().toString());
         int usia = 2016 - tahun;
 
@@ -95,38 +108,80 @@ public class MainActivity extends AppCompatActivity {
 
         int gaji2 = 0;
         String status = null;
-        String ket = null;
+        String ket = "";
         if (rbM.isChecked())
         {
             gaji2 = 500000;
             status = rbM.getText().toString();
-            ket = " Anda mendapat gaji tambahan ";
+            ket = ", Anda mendapat gaji tambahan ";
         }
         else if (rbBM.isChecked())
         {
             status = rbBM.getText().toString();
-            ket = " Anda mendapat gaji tambahan ";
+            ket = ", Anda mendapat gaji tambahan ";
         }
         else if (rbD.isChecked())
         {
             gaji2 = 200000;
             status = rbD.getText().toString();
-            ket = " Anda mendapat gaji tambahan ";
+            ket = ", Anda mendapat gaji tambahan ";
         }
         else if (rbJ.isChecked())
         {
             gaji2 = 200000;
             status = rbJ.getText().toString();
-            ket = " Anda mendapat gaji tambahan ";
+            ket = ", Anda mendapat gaji tambahan ";
         }
+        else
+        {
+            status = "belum memilih";
+        }
+
+        String sekolah = " ";
+        int startlen = sekolah.length();
+        if (cbSD.isChecked()) sekolah += cbSD.getText().toString() + " ; ";
+        if (cbSMP.isChecked()) sekolah += cbSMP.getText().toString() + " ; ";
+        if (cbSMA.isChecked()) sekolah += cbSMA.getText().toString() + " ; ";
+        if (cbD.isChecked()) sekolah += cbD.getText().toString() + " ; ";
+        if (cbS.isChecked()) sekolah += cbS.getText().toString() + " ; ";
+
+        if (sekolah.length()==startlen) sekolah="Anda belum memilih";
 
         int total = gaji1+gaji2;
 
-        tvHasil.setText("Nama anda : " + nama +
-                "\nAnda lahir tahun : "+ spTahun.getSelectedItem().toString() +
-                "\nUmur Anda " + usia +
-                "\nGaji 1 :" + String.valueOf(gaji1) +
-                "\nStatus Anda " + status + ket + String.valueOf(gaji2) +
-                "\nTotal Gaji : " + String.valueOf(total));
+
+
+
+        if (isValid())
+        {
+            nama = etNama.getText().toString();
+
+            tvHasil.setText("Nama Anda : " + nama +
+                    "\nAnda lahir tahun : "+ spTahun.getSelectedItem().toString() +
+                    "\nUmur Anda : " + usia + " tahun" +
+                    "\nJabatan : " + spJabatan.getSelectedItem().toString() +
+                    "\nGaji Pokok dari jabatan : " + String.valueOf(gaji1) +
+                    "\nStatus Anda " + status + ket + String.valueOf(gaji2) +
+                    "\nAnda sudah mencapai pendidikan : " + sekolah +
+                    "\nTotal Gaji : " + String.valueOf(total));
+        }
+    }
+
+    private boolean isValid() {
+        boolean valid = true;
+
+        String namaa = etNama.getText().toString();
+
+        if (namaa.isEmpty())
+        {
+            etNama.setError("Nama belum diisi!");
+            valid = false;
+        }
+        else
+        {
+            etNama.setError(null);
+        }
+
+        return valid;
     }
 }
