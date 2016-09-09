df1bccadc173f4f3d0912c177329a60b0a5c1791
toshiba <gbagoescahyono@gmail.com>
2016-09-09 09:55:49 +0700

    menambahkan spinner(Jabatan Pekerjaan)

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1019/penentugajikaryawan/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xiirpl1019/penentugajikaryawan/MainActivity.java
@@ -3,17 +3,26 @@ package id.sch.smktelkom_mlg.tugas01.xiirpl1019.penentugajikaryawan;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Switch;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText etNama;
     Button btOK;
     TextView tvHasil;
-    Spinner spTahun;
+    Spinner spTahun, spJabatan;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +32,7 @@ public class MainActivity extends AppCompatActivity {
         etNama = (EditText) findViewById(R.id.editTextNama);
         btOK = (Button) findViewById(R.id.buttonOK);
         spTahun = (Spinner) findViewById(R.id.spinnerTahun);
+        spJabatan = (Spinner) findViewById(R.id.spinnerJabatan);
         tvHasil = (TextView) findViewById(R.id.textViewHasil);
 
         /*btOK.setOnClickListener(new View.OnClickListener() {
@@ -35,9 +45,12 @@ public class MainActivity extends AppCompatActivity {
         });*/
 
 
-        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener() {
+
+        findViewById(R.id.buttonOK).setOnClickListener(new View.OnClickListener()
+        {
             @Override
-            public void onClick(View view) {
+            public void onClick(View view)
+            {
                 doClick();
             }
         });
@@ -48,6 +61,33 @@ public class MainActivity extends AppCompatActivity {
         int tahun = Integer.parseInt(spTahun.getSelectedItem().toString());
         int usia = 2016 - tahun;
 
-        tvHasil.setText("Nama anda : " + nama + "\nAnda lahir tahun : " + spTahun.getSelectedItem().toString() + "\nUmur Anda " + usia);
+        String gaji = "";
+        if (spJabatan.getSelectedItem().toString().equals("Customer Service"))
+        {
+            gaji = "2.000.000";
+        }
+        else if (spJabatan.getSelectedItem().toString().equals("Technical Support"))
+        {
+            gaji = "2.500.000";
+        }
+        else if (spJabatan.getSelectedItem().toString().equals("Web Programmer"))
+        {
+            gaji = "4.000.000";
+        }
+        else if (spJabatan.getSelectedItem().toString().equals("Finance and Accounting Officer"))
+        {
+            gaji = "3.000.000";
+        }
+        else if (spJabatan.getSelectedItem().toString().equals("Sales and Marketing Officer"))
+        {
+            gaji = "2.800.000";
+        }
+        else
+        {
+            gaji = "3.500.000";
+        }
+
+        tvHasil.setText("Nama anda : " + nama + "\nAnda lahir tahun : "+ spTahun.getSelectedItem().toString() +"\nUmur Anda " + usia
+        + "\nGaji :" + gaji);
     }
 }
